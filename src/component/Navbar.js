43dc92e7e744cf63b58392544c628dbd2fe2feee
Ruby Yeh <hyoka4654@gmail.com>
2021-11-14 19:40:30 +0800

    second

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -80,18 +80,53 @@ export default function NavBar() {
                     <li className="li" >
                         <NavItem to="/favorite" > <img className="navlist-icon" src={like_icon} /></NavItem>
                     </li>
-                    <li className="li"><a href="#">About</a>
-                        <ul className="ul">
-                            <li className="li"><a href="">Our team</a></li>
-                            <li className="li"><a href="">History</a></li>
-                        </ul>
+                    <li className="li">
+                        <div onMouseEnter={toggle}>
+                            <NavItem to="/cart">
+                                <Badge showZero={true} count={counts} size={"small"}>
+                                    <img className="navlist-icon" src={cart_icon} />
+                                </Badge>
+                            </NavItem>
+                        </div>
                     </li>
-                    <li className="li"><a href="#">Clients</a></li>
-                    <li className="li"><a href="#">Contact Us</a></li>
+                    <li className="li"><a href="#">Log in</a></li>
+                    <li className="li li-btn"> 
+                        <NavItem to="/shop/women_bottom" >Sign up</NavItem>
+                    </li>
                 </ul>
 
             </nav>
 
+            {
+                counts > 0 ?
+                    <div onMouseLeave={toggle} className={ClassName}>
+                        {cartItems.map(product => (
+                            <div className="collapse-item">
+                                <img className="collapse-item-img" src={product.imgUrl_1} />
+                                <p className="collapse-item-name">{product.name}</p>
+                                <p className="collapse-item-price">NT {product.price}</p>
+                            </div>
+                        ))}
+                        <div className="collapse-bottom">
+                            <p className="collapse-total" >共{counts}件商品</p>
+                            <span className="collapse-button">
+                                <NavItem to="/cart">
+                                    查看我的購物車
+                                </NavItem>
+                            </span>
+                        </div>
+                    </div>
+
+                    :
+                    <div onMouseLeave={toggle} className={ClassName}>
+                        <p>目前購物車沒有商品</p>
+                        <p>共{counts}件商品</p>
+                        <NavItem to="/cart">
+                            查看我的購物車
+                        </NavItem>
+                    </div>
+
+            }
         </div>
     )
 
--- src/component/ProductDetail.js
+++ src/component/ProductDetail.js
@@ -1,57 +1,63 @@
 import { useContext } from "react";
 import { StoreContext } from "../store"
-import { Row ,Col,Button} from "antd";
+import { Row, Col, Button } from "antd";
 import AddToCart from "./AddToCart";
-
+import chat from '../assests/Icon/chat-bubble.png';
+import like from '../assests/Icon/like.png'
+import Productlist from "./Productlist";
+import anotherProduct from '../json/women_bottom.json';
 
 export default function ProductDetail() {
     const { state: { productDetail: { product } }, dispatch } = useContext(StoreContext);
 
     return (
-        <div className="productdetail_container">
-            <div className="productdetail_img">
-                <img className="productdetail_img_img" src={product.imgUrl_1}/>
-            </div>
-            <div className="productdetail_detail">
-                <div className="productdetail_detail_detail">
-                    <div className="productdetail_seller">
-                        <img className="productdetail_sellericon" src={product.imgUrl_1} alt="" />
-                        <div className="productdetail_sellername">{product.seller_name}</div>
-                    </div>
-                    <div className="productdetail_title">{product.name}</div>
-                    <div className="productdetail_price">NT {product.price}</div>
-                    <div className="productdetail_button">
-                        <img className="productdetail_icon" src="img/e-commerce-like-heart.png" alt="" />
-                        <img className="productdetail_icon" src="img/chat-bubble.png" alt="" />
-                    </div>
-                    <AddToCart/>
-                    <div className="productdetail_information">
-                        <div className="productdetail_item">尺寸</div>
-                        <div className="productdetail_answer">{product.size}</div>
-                    </div>
-                    <div className="productdetail_information">
-                        <div className="productdetail_item">品牌</div>
-                        <div className="productdetail_answer">{product.brand}</div>
-                    </div>
-                    <div className="productdetail_information">
-                        <div className="productdetail_item">狀況</div>
-                        <div className="productdetail_answer">{product.content}</div>
-                    </div>
-                    <div className="productdetail_information">
-                        <div className="productdetail_item">顏色</div>
-                        <div className="productdetail_answer">{product.color}</div>
-                    </div>
-                    <div className="productdetail_information">
-                        <div className="productdetail_item">標籤</div>
-                        <div className="productdetail_answer">{product.tag}</div>
-                    </div>
-
-
-
+        <>
+            <div className="productdetail_container">
+                <div className="productdetail_img">
+                    <img className="productdetail_img_img" src={product.imgUrl_1} />
                 </div>
-            </div>
+                <div className="productdetail_detail">
+                    <div className="productdetail_detail_detail">
+                        <div className="productdetail_seller">
+                            <img className="productdetail_sellericon" src={product.imgUrl_1} alt="" />
+                            <div className="productdetail_sellername">{product.seller_name}</div>
+                        </div>
+                        <div className="productdetail_title">{product.name}</div>
+                        <div className="productdetail_price">NT {product.price}</div>
+                        <div className="productdetail_button">
+                            <img className="productdetail_icon" src={like} alt="" />
+                            <img className="productdetail_icon" src={chat} alt="" />
+                        </div>
+                        <AddToCart />
+                        <div className="productdetail_information">
+                            <div className="productdetail_item">尺寸</div>
+                            <div className="productdetail_answer">{product.size}</div>
+                        </div>
+                        <div className="productdetail_information">
+                            <div className="productdetail_item">品牌</div>
+                            <div className="productdetail_answer">{product.brand}</div>
+                        </div>
+                        <div className="productdetail_information">
+                            <div className="productdetail_item">狀況</div>
+                            <div className="productdetail_answer">{product.content}</div>
+                        </div>
+                        <div className="productdetail_information">
+                            <div className="productdetail_item">顏色</div>
+                            <div className="productdetail_answer">{product.color}</div>
+                        </div>
+                        <div className="productdetail_information">
+                            <div className="productdetail_item">標籤</div>
+                            <div className="productdetail_answer">{product.tag}</div>
+                        </div>
 
-        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+            <Productlist products={anotherProduct} text="賣家其他商品"/>
+        </>
     )
 
 }
